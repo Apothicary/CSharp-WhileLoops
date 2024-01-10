64a92a7b53a2ba2cb81d4babc1d8ee378c464e2c
Nathan Strang <n.strang95@gmail.com>
2024-01-10 09:06:34 +0000

    Adding program code

---
+++ WhileLoops/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace WhileLoops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // A while loop will repeat code indefintely while a condition remains true 
+
+            Console.Write("Enter your name: ");
+            String name = Console.ReadLine();
+
+            /* Another shorter way to do the above would be :
+             *
+             *  String name = "";
+             *  
+            */
+            while (name == "")      // Start of While Loop if name String is left blank
+            {
+                Console.Write("Enter your name: ");
+                name = Console.ReadLine();
+            }
+
+            Console.WriteLine("Hello " + name);
+
+            Console.ReadKey();
+        }
+    }
+}
